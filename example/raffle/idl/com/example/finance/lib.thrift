c0bf4b02e2f4647a5ea2c0ee0797e2c90bbfa3d1
Navneeth Jayendran <navneejay@gmail.com>
2021-02-06 19:03:32 -0800

    Prove edge case behavior better

--- example/raffle/idl/com/example/finance/lib.thrift
+++ example/raffle/idl/com/example/finance/lib.thrift
@@ -1,5 +1,9 @@
 namespace java com.example.finance
 
 struct Foo {
-  1: string no_thanks;
+  1: string finance_foo;
+}
+
+struct Bar {
+  1: string bar_field;
 }
--- example/raffle/idl/com/example/raffle/raffle_service.thrift
+++ example/raffle/idl/com/example/raffle/raffle_service.thrift
@@ -2,14 +2,18 @@ namespace java com.example.raffle
 
 include 'shared.thrift'
 // It seems like when file names conflict, the last imported file with the matching symbol definition
-// is used
+// is used; in order to find the canonical name of a symbol in cases like this, you need to inspect the
+// contents of all matching files to resolve the last file with the symbol defined
 include 'com/example/finance/lib.thrift'
 include 'com/example/library/lib.thrift'
 
 struct SayHelloRequest {
   1: shared.Context context;
   2: string name;
+  // resolves com.example.finance.Foo
   3: lib.Foo foo;
+  // resolves to com.example.library.Foo
+  4: lib.Bar bar;
 }
 
 struct SayHelloResponse {
